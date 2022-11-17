aa378e6bf9492c124174c68b867729b7c1160b8f
jhara islam <103064101+jharaislam19@users.noreply.github.com>
2022-11-17 08:33:22 +0600

    add some

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -16,3 +16,13 @@ const firebaseConfig = {
 /// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
+
+
+//.env.local file likhte hobe
+
+// REACT_APP_apiKey=AIzaSyABdsFcdOpqLmfSnwkCpP9iZH6KfU3kEQc
+// REACT_APP_authDomain=practice-for-email.firebaseapp.com
+// REACT_APP_projectId=practice-for-email
+// REACT_APP_storageBucket=practice-for-email.appspot.com
+// REACT_APP_messagingSenderId=974715361266
+// REACT_APP_appId=1:974715361266:web:9bd55af13a786249ecbd6b
